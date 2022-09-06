2e3af0cd7033da554d0f75bee88229af1627db5c
djs2082 <dilipjoshis98@gmail.com>
2022-09-06 22:47:37 +0530

    integrated api with code for showing logged in users information

--- src/components/dashboard/userData.tsx
+++ src/components/dashboard/userData.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Avatar } from 'flowbite-react'
+import { getUserInfo } from '../../services/serverCalls'
 import avatar1 from '../../styles/assets/avatar/avatar1.jpeg'
 
 function UserData() {
+  const [userData, setUserData] = useState({name:null, email:null})
+  useEffect(()=>{
+    getUserInfo()
+    .then((response) => {
+      setUserData({name: response.data.name, email: response.data.email})
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  },[]) 
   return (
     <div className="w-full h-[15px] lg:h-[175px]">
       <div className="flex lg:pt-8 ml-5 lg:ml-10 space-x-6">
@@ -14,8 +25,8 @@ function UserData() {
           />          
         </div>
         <div className="my-auto">
-          <h1 className="text-3xl font-lato font-semibold">Hello Ashirwad 👋</h1>
-          <h2 className="text-md font-lato font-semibold text-gray-500">ashirwadpramodshetye@gmail.com</h2>
+          <h1 className="text-3xl font-lato font-semibold">Hello {userData.name} 👋</h1>
+          <h2 className="text-md font-lato font-semibold text-gray-500">{userData.email}</h2>
         </div>
       </div>
     </div>
