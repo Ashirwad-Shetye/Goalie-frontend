646e708694657fdfdafa1d14df1196fb1fd84bb4
djs2082 <dilipjoshis98@gmail.com>
2022-09-07 01:50:47 +0530

    Integrated Api For showing all the goals in timeline sheet

--- src/components/dashboard/timeline.tsx
+++ src/components/dashboard/timeline.tsx
@@ -1,78 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Timeline } from 'flowbite-react'
+import { getGoalData } from '../../services/serverCalls'
 
 function TimelineCard() {
+
+    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+    const [goals, setGoals] = useState<
+    Array<{
+        text: string,
+        description: string,
+        lastDate: string
+    }>
+>([])
+    useEffect(()=>{
+        getGoalData()
+        .then((response) => {
+            console.log(response)
+            setGoals([...response.data.pendingGoals, ...response.data.finishedGoals]);
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    },[])
   return (
     <div className="mt-48 lg:mt-5 lg:mr-10">
         <div className="text-3xl font-poppins font-bold mt-2 mb-3 ml-10 text-orange-500">Goal Timeline</div>
         <div className="justify-center w-full mx-auto align-middle h-80 px-12 overflow-x-clip lg: 
         overflow-scroll py-5 scrollbar-thin scrollbar-thumb-11 scrollbar-thumb-orange-400 scrollbar-track-transparent overflow-y-scroll
         scrollbar-thumb-rounded-full scrollbar-track-rounded-full"> 
-            <Timeline>
-                <Timeline.Item>
+        <Timeline>
+        {
+            goals.map((goal)=>{
+                return(
+                    <Timeline.Item>
                     <Timeline.Point />
                     <Timeline.Content>
                     <Timeline.Time>
                         <div className="text-lg text-slate-600">
-                            February 2022
+                            {new Date(goal.lastDate).getDate()} {month[new Date(goal.lastDate).getMonth()]} {new Date(goal.lastDate).getFullYear()}
                         </div>
                     </Timeline.Time>
                     <Timeline.Title>
-                        Application UI code in Tailwind CSS
+                        {goal.text}
                     </Timeline.Title>
                     <Timeline.Body>
-                        Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.
+                        {goal.description}
                     </Timeline.Body>
                     </Timeline.Content>
                 </Timeline.Item>
-                <Timeline.Item>
-                    <Timeline.Point />
-                    <Timeline.Content>
-                    <Timeline.Time>
-                        <div className="text-lg text-slate-600">
-                            March 2022
-                        </div>
-                    </Timeline.Time>
-                    <Timeline.Title>
-                        Marketing UI design in Figma
-                    </Timeline.Title>
-                    <Timeline.Body>
-                        All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.
-                    </Timeline.Body>
-                    </Timeline.Content>
-                </Timeline.Item>
-                <Timeline.Item>
-                    <Timeline.Point />
-                    <Timeline.Content>
-                    <Timeline.Time>
-                        <div className="text-lg text-slate-600">
-                            April 2022
-                        </div>
-                    </Timeline.Time>
-                    <Timeline.Title>
-                        E-Commerce UI code in Tailwind CSS
-                    </Timeline.Title>
-                    <Timeline.Body>
-                        Get started with dozens of web components and interactive elements built on top of Tailwind CSS.
-                    </Timeline.Body>
-                    </Timeline.Content>
-                </Timeline.Item>
-                <Timeline.Item>
-                    <Timeline.Point />
-                    <Timeline.Content>
-                    <Timeline.Time>
-                        <div className="text-lg text-slate-600">
-                            April 2022
-                        </div>
-                    </Timeline.Time>
-                    <Timeline.Title>
-                        E-Commerce UI code in Tailwind CSS
-                    </Timeline.Title>
-                    <Timeline.Body>
-                        Get started with dozens of web components and interactive elements built on top of Tailwind CSS.
-                    </Timeline.Body>
-                    </Timeline.Content>
-                </Timeline.Item>
+                )
+            })
+        }
+
             </Timeline>
         </div>
     </div>
