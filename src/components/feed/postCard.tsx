cb5edd46216fd1d5891b29a0dc2a271864688c69
Ashirwad P Shetye <ashirwadpramodshetye@gmail.com>
2022-09-09 22:10:22 +0530

    Post card UX complete

--- src/components/feed/postCard.tsx
+++ src/components/feed/postCard.tsx
@@ -4,7 +4,7 @@ import Post from './post'
 function PostCard() {
   return (
     <div className=''>
-        <div className='bg-blue-200 container mt-40 md:mt-0 mx-auto rounded-3xl justify-center align-middle h-[540px] w-[360px]
+        <div className='container mt-40 md:mt-0 mx-auto rounded-3xl justify-center align-middle h-[540px] w-[360px]
             shadow-lg overflow-y-scroll scrollbar-hide'>
                <Post/>
                <Post/>
