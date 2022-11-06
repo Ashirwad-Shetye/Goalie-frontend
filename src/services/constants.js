7e3b6dae1fc9377a77407b968eaabe5c602d0bf1
Ashirwad P Shetye <ashirwadpramodshetye@gmail.com>
2022-11-06 07:52:36 +0530

    added constants

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,15 +1,21 @@
-export const DEFAULT_URL = process.env.API_URL || 'http://localhost:5000'
+export const DEFAULT_URL = process.env.API_URL || "http://localhost:5000";
 
-export const authHeaderKeys = ['platform', 'Authorization']
+export const authHeaderKeys = ["platform", "Authorization"];
 
-export const USER_LOGIN_URL = '/api/users/login'
+export const USER_LOGIN_URL = "/api/users/login";
 
-export const USER_REGISTER_URL = '/api/users'
+export const USER_REGISTER_URL = "/api/users";
 
-export const USER_SET_GOAL = '/api/goals'
+export const USER_SET_GOAL = "/api/goals";
 
-export const GET_GOALS_DATA = '/api/goals/goalStats'
+export const GET_GOALS_DATA = "/api/goals/goalStats";
 
-export const GET_MY_INFO = '/api/users/me'
+export const GET_MY_INFO = "/api/users/me";
 
-export const GET_USER_GOALS = '/api/goals/'
+export const GET_USER_GOALS = "/api/goals/";
+
+export const SET_NEW_POSTS = "/api/posts";
+
+export const GET_ALL_POSTS = "/api/posts";
+
+export const LIKE_POST = "/api/posts/";
