8800e9b0a8ecbc2bd3a7f280e7b44fb7ca0f1418
Ashirwad P Shetye <ashirwadpramodshetye@gmail.com>
2023-02-20 01:57:37 +0530

    created new navbar with different ui

--- src/common/navbar.tsx
+++ src/common/navbar.tsx
@@ -1,9 +1,102 @@
 import React from "react";
+import logo from "../styles/assets/logo/logo.png";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { CgFeed } from "react-icons/cg";
+import { AiOutlineTeam } from "react-icons/ai";
+import { BiChevronDown } from "react-icons/bi";
+import { HiOutlineLogout } from "react-icons/hi";
+import avatar_24 from "../styles/assets/avatar/avatar_24.png";
+import { Tooltip } from "flowbite-react";
 
 function Navbar() {
   return (
-    <div>
-      <div></div>
+    <div className="h-full border flex justify-between md:grid md:grid-cols-none md:grid-rows-6">
+      <div className="md:pt-2 md:row-span-1">
+        <img src={logo} alt="goalie logo" className="w-28 md:w-auto" />
+      </div>
+      <div
+        className="h-full hidden row-span-4 bg-orange-400 bg-opacity-25 min-h-[20rem] w-fit text-2xl 
+      mx-auto md:flex flex-col justify-between rounded-full"
+      >
+        <div className="h-[40%] px-1 my-1 mx-auto min-h-[8rem] flex flex-col justify-between">
+          <Tooltip
+            content="Dashboard"
+            style="light"
+            animation="duration-300"
+            placement="right"
+            arrow={false}
+          >
+            <div
+              className="hover:bg-orange-400 hover:bg-opacity-60 flex justify-center items-center 
+          rounded-full w-12 h-12 cursor-pointer duration-150 text-gray-600 hover:text-white"
+            >
+              <MdOutlineSpaceDashboard />
+            </div>
+          </Tooltip>
+          <Tooltip
+            content="Social Feed"
+            placement="right"
+            style="light"
+            animation="duration-300"
+            arrow={false}
+          >
+            <div
+              className="hover:bg-orange-400 hover:bg-opacity-60 flex justify-center items-center 
+          rounded-full w-12 h-12 cursor-pointer duration-150 text-gray-600 hover:text-white"
+            >
+              <CgFeed />
+            </div>
+          </Tooltip>
+          <Tooltip
+            content="About us"
+            placement="right"
+            style="light"
+            animation="duration-300"
+            arrow={false}
+          >
+            <div
+              className="hover:bg-orange-400 hover:bg-opacity-60 flex justify-center items-center 
+          rounded-full w-12 h-12 cursor-pointer duration-150 text-gray-600 hover:text-white"
+            >
+              <AiOutlineTeam />
+            </div>
+          </Tooltip>
+        </div>
+        <div className="h-fit my-1 mx-auto">
+          <Tooltip
+            content="Logout"
+            placement="right"
+            style="light"
+            animation="duration-300"
+            arrow={false}
+          >
+            <div
+              className="hover:bg-orange-400 hover:bg-opacity-60 flex justify-center items-center 
+          rounded-full w-12 h-12 cursor-pointer duration-150 text-gray-400 hover:text-white"
+            >
+              <HiOutlineLogout />
+            </div>
+          </Tooltip>
+        </div>
+      </div>
+      <div
+        className="md:hidden hover:shadow-2xl hover:scale-105 duration-150 cursor-pointer shadow-xl mx-3 
+      rounded-full flex my-auto w-[11rem] justify-between bg-white"
+      >
+        <div className="flex">
+          <div className="w-12 p-1">
+            <img src={avatar_24} alt="avatar" />
+          </div>
+          <div className="px-2 flex items-center justify-center">
+            <h1 className="w-[5rem] font-poppins truncate text-gray-500">
+              Ashirwad shetye
+            </h1>
+          </div>
+        </div>
+        <div className="pr-1 flex items-center justify-center text-gray-400 text-2xl">
+          <BiChevronDown />
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -13,10 +13,15 @@ function Dashboard() {
   return (
     <div className="bg-dashboard bg-right bg-no-repeat bg-cover h-screen min-h-[640px] bg-fixed">
       <div className="h-full md:flex">
-        <div className="relative border h-20 w-full md:h-full md:w-[8rem]">
+        <div className="relative h-16 w-full md:h-full md:w-[8rem]">
           <Navbar />
         </div>
         <main className="max-w-[1200px] w-full md:w-11/12 md:h-full min-w-screen mx-auto flex flex-col justify-center items-center">
+          <div className="hidden w-full h-16 md:flex items-end">
+            <h1 className="font-poppins text-2xl ml-10 text-gray-300 hover:text-gray-500 duration-150">
+              Dashboard
+            </h1>
+          </div>
           <div className="w-10/12 md:w-8/12 lg:w-11/12 mx-auto grid grid-cols-5 justify-center align-middle my-auto">
             <section className="hidden lg:col-span-2 space-y-5 lg:relative lg:grid grid-rows-4 content-between w-full">
               <Suspense>
