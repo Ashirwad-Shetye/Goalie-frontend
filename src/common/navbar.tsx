ccbd4fda5df4c6aef687bfb75cd8fbe459459bab
Ashirwad P Shetye <ashirwadpramodshetye@gmail.com>
2023-02-20 03:34:35 +0530

    menu with dropdown for small screen

--- src/common/navbar.tsx
+++ src/common/navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../styles/assets/logo/logo.png";
 import { MdOutlineSpaceDashboard } from "react-icons/md";
 import { CgFeed } from "react-icons/cg";
@@ -9,6 +9,15 @@ import avatar_24 from "../styles/assets/avatar/avatar_24.png";
 import { Tooltip } from "flowbite-react";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const handleMenu = () => {
+    if (open) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  };
   return (
     <div className="h-full flex justify-between md:grid md:grid-cols-none md:grid-rows-6">
       <div className="md:pt-2 md:row-span-1">
@@ -80,22 +89,37 @@ function Navbar() {
         </div>
       </div>
       <div
-        className="md:hidden hover:shadow-2xl hover:scale-105 duration-150 cursor-pointer shadow-xl mx-3 
-      rounded-full flex my-auto w-[11rem] justify-between bg-white"
+        onClick={handleMenu}
+        className="md:hidden cursor-pointer shadow-xl mx-3 
+      rounded-full flex flex-col my-auto w-[11rem] justify-between bg-white"
       >
-        <div className="flex">
-          <div className="w-12 p-1">
-            <img src={avatar_24} alt="avatar" />
+        <div className="flex justify-between items-center hover:scale-105 duration-150">
+          <div className="flex">
+            <div className="w-12 p-1">
+              <img src={avatar_24} alt="avatar" />
+            </div>
+            <div className="px-2 flex items-center justify-center">
+              <h1 className="w-[5rem] font-poppins truncate text-gray-500">
+                Ashirwad shetye
+              </h1>
+            </div>
           </div>
-          <div className="px-2 flex items-center justify-center">
-            <h1 className="w-[5rem] font-poppins truncate text-gray-500">
-              Ashirwad shetye
-            </h1>
+          <div className="pr-1 flex items-center justify-center text-gray-400 text-2xl">
+            <BiChevronDown />
           </div>
         </div>
-        <div className="pr-1 flex items-center justify-center text-gray-400 text-2xl">
-          <BiChevronDown />
-        </div>
+        {open ? (
+          <div className="absolute justify-center items-center top-[4.3rem] rounded-md w-[9rem] mx-5 bg-white shadow-lg">
+            <ul>
+              <li>Dashboard</li>
+              <li>Feed</li>
+              <li>About Us</li>
+              <li>Logout</li>
+            </ul>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
