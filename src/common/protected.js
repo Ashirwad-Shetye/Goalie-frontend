eba4f2feb46f7743891f0e9abe40aec7736f527c
Ashirwad P Shetye <ashirwadpramodshetye@gmail.com>
2023-02-24 04:11:41 +0530

    create goal page UI layout changed for new navbar

--- src/common/protected.js
+++ src/common/protected.js
@@ -1,10 +1,9 @@
 import { Navigate } from "react-router-dom";
 import { getItemFromStorage } from "../services/storage";
 const Protected = ({ fallBackRoute, isLoggedIn, children }) => {
-    console.log(isLoggedIn)
- if (!isLoggedIn) {
- return <Navigate to={fallBackRoute} replace />;
- }
- return children;
+  if (!isLoggedIn) {
+    return <Navigate to={fallBackRoute} replace />;
+  }
+  return children;
 };
 export default Protected;
--- src/pages/creategoal.tsx
+++ src/pages/creategoal.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { useRef, useState } from "react";
+import { useRef, useState, Suspense } from "react";
 import { useNavigate } from "react-router-dom";
-import Navs from "../components/creategoal/navs";
-import Header from "../common/header";
 import { setGoal } from "./../services/serverCalls";
 import { ThreeDots } from "react-loader-spinner";
+import Navbar from "../common/navbar";
+import avatar_24 from "../styles/assets/avatar/avatar_24.png";
 
 function CreateGoal() {
   const navigate = useNavigate();
@@ -103,176 +103,191 @@ function CreateGoal() {
   };
 
   return (
-    <div
-      className="bg-dashboard bg-right bg-no-repeat bg-cover bg-fixed 
-    min-h-screen min-w-screen max-h-fit max-w-screen"
-    >
-      <main className="min-w-screen max-w-[1200px] mx-auto">
-        <Header>
-          <Navs />
-        </Header>
-        <div className="mx-auto mt-10 md:w-3/5 lg:w-2/5">
-          <div className="text-3xl font-poppins text-center text-orange-500 my-6">
-            Let's get started with your goal
-          </div>
-          <form
-            onSubmit={onSubmit}
-            className="container flex flex-col w-[360px] md:w-[400px] mx-auto"
-          >
-            <div className="flex flex-col">
-              <label className="text-xl text-gray-600 my-4 font-lato font-semibold text-center">
-                I commit to
-              </label>
-              <input
-                className="placeholder:text-slate-400 mx-auto py-2 md:p-2 mb-2 w-10/12 border-0 text-center
-                            focus:border-orange-500 focus:ring-0 placeholder:text-center shadow-inner rounded-lg"
-                type="text"
-                name="goal"
-                maxLength={42}
-                onChange={onChange}
-                placeholder="Enter a goal"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label className="text-xl text-gray-600 my-4 font-lato font-semibold text-center">
-                I want to complete this goal by
-              </label>
-              <input
-                className="placeholder:text-slate-400 text-slate-400 mx-auto py-2 md:p-2 mb-2 w-10/12 border-0 text-center
-                            focus:border-orange-500 focus:ring-0 placeholder:text-center shadow-inner rounded-lg cursor-text"
-                type="date"
-                onChange={onChange}
-                name="date"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label className="text-xl text-gray-600 my-4 font-lato font-semibold text-center">
-                Describe your goal
-              </label>
-              <textarea
-                className="placeholder:text-slate-400 mx-auto py-2 md:p-2 mb-2 w-10/12 border-0 text-center resize-y
-                            focus:border-orange-500 focus:ring-0 placeholder:text-center shadow-inner rounded-lg scrollbar-hide "
-                name="description"
-                ref={textRef}
-                onChange={onChangeHandler}
-                placeholder="describe your goal"
-                maxLength={280}
-              ></textarea>
-            </div>
-            <div className="flex flex-col">
-              <label className="text-xl text-gray-600 my-4 font-lato font-semibold text-center">
-                Remind me
-              </label>
-              <div className="container text-center mx-auto flex w-10/12 justify-evenly space-x-6 md:space-x-6">
-                {/* Daily */}
-                {buttonSelected1 ? (
-                  <button
-                    type="button"
-                    value={1}
-                    onClick={onRemaindChange1}
-                    name="remaind"
-                    className="text-xl h-10 duration-200 bg-orange-500
-                                border-orange-500 text-white hover:scale-105 rounded-xl border-2 w-24"
-                  >
-                    Daily
-                  </button>
-                ) : (
-                  <button
-                    type="button"
-                    value={1}
-                    onClick={onRemaindChange1}
-                    name="remaind"
-                    className="text-xl h-10 hover:text-white hover:bg-orange-500 duration-200 
-                                border-orange-200 hover:border-orange-500 hover:scale-105 rounded-xl border-2 w-24"
-                  >
-                    Daily
-                  </button>
-                )}
-                {/* Weekly */}
-                {buttonSelected2 ? (
-                  <button
-                    type="button"
-                    value={1}
-                    onClick={onRemaindChange2}
-                    name="remaind"
-                    className="text-xl h-10 duration-200 bg-orange-500
-                                border-orange-500 text-white hover:scale-105 rounded-xl border-2 w-24"
-                  >
-                    Weekly
-                  </button>
-                ) : (
-                  <button
-                    type="button"
-                    value={1}
-                    onClick={onRemaindChange2}
-                    name="remaind"
-                    className="text-xl h-10 hover:text-white hover:bg-orange-500 duration-200 
-                                border-orange-200 hover:border-orange-500 hover:scale-105 rounded-xl border-2 w-24"
-                  >
-                    Weekly
-                  </button>
-                )}
-                {/* Monthly */}
-                {buttonSelected3 ? (
-                  <button
-                    type="button"
-                    value={1}
-                    onClick={onRemaindChange3}
-                    name="remaind"
-                    className="text-xl h-10 duration-200 bg-orange-500
-                                border-orange-500 text-white hover:scale-105 rounded-xl border-2 w-24"
-                  >
-                    Monthly
-                  </button>
-                ) : (
-                  <button
-                    type="button"
-                    value={1}
-                    onClick={onRemaindChange3}
-                    name="remaind"
-                    className="text-xl h-10 hover:text-white hover:bg-orange-500 duration-200 
-                                border-orange-200 hover:border-orange-500 hover:scale-105 rounded-xl border-2 w-24"
-                  >
-                    Monthly
-                  </button>
-                )}
+    <div className="bg-dashboard bg-right bg-no-repeat bg-cover h-screen min-h-[640px] bg-fixed z-0">
+      <div className="h-full md:flex">
+        <div className="relative h-[10vh] min-h-16 w-full md:h-full md:w-[8rem] z-50 md:z-40">
+          <Suspense>
+            <Navbar />
+          </Suspense>
+        </div>
+        <main className="min-w-screen w-11/12 max-w-[1200px] mx-auto">
+          <div className="hidden w-full h-16 md:flex items-end justify-between">
+            <h1 className="font-poppins text-2xl ml-10 text-gray-300 hover:text-gray-500 duration-150">
+              Dashboard
+            </h1>
+            <div className="shadow-lg -mb-2 mr-10 rounded-full flex w-[11rem] justify-between bg-white">
+              <div className="flex">
+                <div className="w-12 p-1">
+                  <img src={avatar_24} alt="avatar" />
+                </div>
+                <div className="px-2 flex items-center justify-center">
+                  <h1 className="w-[7rem] font-poppins truncate text-gray-500">
+                    Ashirwad shetye
+                  </h1>
+                </div>
               </div>
             </div>
-            {/* <button type="submit"
-                        className='text-xl mx-auto mt-12 font-lato bg-orange-500 
-                            text-white w-32 font-bold py-1.5 px-4 rounded-2xl hover:bg-transparent 
-                            hover:text-orange-500 duration-200 border-orange-500 border-2 hover:shadow-none'
-                    >Submit</button> */}
-            {isButtonLoading ? (
-              <button
-                type="submit"
-                className="text-xl my-4 mx-auto mt-12 font-lato bg-orange-400 h-12 w-32 cursor-default 
-                             py-1.5 px-4 rounded-2xl border-orange-400 duration-200 border-2"
+          </div>
+          <div className="mx-auto md:w-10/12 lg:w-8/12 h-[90vh] flex items-center justify-center">
+            <div className="">
+              <div className="text-3xl font-poppins text-center text-orange-500 my-6">
+                Let's get started with your goal
+              </div>
+              <form
+                onSubmit={onSubmit}
+                className="container flex flex-col w-[360px] md:w-[400px] mx-auto"
               >
-                <div className="w-fit mx-auto">
-                  <ThreeDots
-                    height="15"
-                    width="70"
-                    radius="3"
-                    color="#ffffff"
-                    ariaLabel="three-dots-loading"
-                    visible={true}
+                <div className="flex flex-col">
+                  <label className="text-xl text-gray-600 my-4 font-lato font-semibold text-center">
+                    I commit to
+                  </label>
+                  <input
+                    className="placeholder:text-slate-400 mx-auto py-2 md:p-2 mb-2 w-10/12 border-0 text-center
+                            focus:border-orange-500 focus:ring-0 placeholder:text-center shadow-inner rounded-lg"
+                    type="text"
+                    name="goal"
+                    maxLength={42}
+                    onChange={onChange}
+                    placeholder="Enter a goal"
                   />
                 </div>
-              </button>
-            ) : (
-              <button
-                type="submit"
-                className="text-xl my-4 mx-auto mt-12 font-lato bg-orange-500 h-12 w-32 
+                <div className="flex flex-col">
+                  <label className="text-xl text-gray-600 my-4 font-lato font-semibold text-center">
+                    I want to complete this goal by
+                  </label>
+                  <input
+                    className="placeholder:text-slate-400 text-slate-400 mx-auto py-2 md:p-2 mb-2 w-10/12 border-0 text-center
+                            focus:border-orange-500 focus:ring-0 placeholder:text-center shadow-inner rounded-lg cursor-text"
+                    type="date"
+                    onChange={onChange}
+                    name="date"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label className="text-xl text-gray-600 my-4 font-lato font-semibold text-center">
+                    Describe your goal
+                  </label>
+                  <textarea
+                    className="placeholder:text-slate-400 mx-auto py-2 md:p-2 mb-2 w-10/12 border-0 text-center resize-y
+                            focus:border-orange-500 focus:ring-0 placeholder:text-center shadow-inner rounded-lg scrollbar-hide "
+                    name="description"
+                    ref={textRef}
+                    onChange={onChangeHandler}
+                    placeholder="describe your goal"
+                    maxLength={280}
+                  ></textarea>
+                </div>
+                <div className="flex flex-col">
+                  <label className="text-xl text-gray-600 my-4 font-lato font-semibold text-center">
+                    Remind me
+                  </label>
+                  <div className="container text-center mx-auto flex w-10/12 justify-evenly space-x-6 md:space-x-6">
+                    {/* Daily */}
+                    {buttonSelected1 ? (
+                      <button
+                        type="button"
+                        value={1}
+                        onClick={onRemaindChange1}
+                        name="remaind"
+                        className="text-xl h-10 duration-200 bg-orange-500
+                                border-orange-500 text-white hover:scale-105 rounded-xl border-2 w-24"
+                      >
+                        Daily
+                      </button>
+                    ) : (
+                      <button
+                        type="button"
+                        value={1}
+                        onClick={onRemaindChange1}
+                        name="remaind"
+                        className="text-xl h-10 hover:text-white hover:bg-orange-500 duration-200 
+                                border-orange-200 hover:border-orange-500 hover:scale-105 rounded-xl border-2 w-24"
+                      >
+                        Daily
+                      </button>
+                    )}
+                    {/* Weekly */}
+                    {buttonSelected2 ? (
+                      <button
+                        type="button"
+                        value={1}
+                        onClick={onRemaindChange2}
+                        name="remaind"
+                        className="text-xl h-10 duration-200 bg-orange-500
+                                border-orange-500 text-white hover:scale-105 rounded-xl border-2 w-24"
+                      >
+                        Weekly
+                      </button>
+                    ) : (
+                      <button
+                        type="button"
+                        value={1}
+                        onClick={onRemaindChange2}
+                        name="remaind"
+                        className="text-xl h-10 hover:text-white hover:bg-orange-500 duration-200 
+                    border-orange-200 hover:border-orange-500 hover:scale-105 rounded-xl border-2 w-24"
+                      >
+                        Weekly
+                      </button>
+                    )}
+                    {/* Monthly */}
+                    {buttonSelected3 ? (
+                      <button
+                        type="button"
+                        value={1}
+                        onClick={onRemaindChange3}
+                        name="remaind"
+                        className="text-xl h-10 duration-200 bg-orange-500
+                    border-orange-500 text-white hover:scale-105 rounded-xl border-2 w-24"
+                      >
+                        Monthly
+                      </button>
+                    ) : (
+                      <button
+                        type="button"
+                        value={1}
+                        onClick={onRemaindChange3}
+                        name="remaind"
+                        className="text-xl h-10 hover:text-white hover:bg-orange-500 duration-200 
+                                border-orange-200 hover:border-orange-500 hover:scale-105 rounded-xl border-2 w-24"
+                      >
+                        Monthly
+                      </button>
+                    )}
+                  </div>
+                </div>
+                {isButtonLoading ? (
+                  <button
+                    type="submit"
+                    className="text-xl my-4 mx-auto mt-12 font-lato bg-orange-400 h-12 w-32 cursor-default 
+                             py-1.5 px-4 rounded-2xl border-orange-400 duration-200 border-2"
+                  >
+                    <div className="w-fit mx-auto">
+                      <ThreeDots
+                        height="15"
+                        width="70"
+                        radius="3"
+                        color="#ffffff"
+                        ariaLabel="three-dots-loading"
+                        visible={true}
+                      />
+                    </div>
+                  </button>
+                ) : (
+                  <button
+                    type="submit"
+                    className="text-xl my-4 mx-auto mt-12 font-lato bg-orange-500 h-12 w-32 
                             text-white font-bold py-1.5 px-4 rounded-2xl hover:bg-transparent 
                             hover:text-orange-500 border-orange-500 duration-200 border-2 hover:shadow-none"
-              >
-                Submit
-              </button>
-            )}
-          </form>
-        </div>
-      </main>
+                  >
+                    Submit
+                  </button>
+                )}
+              </form>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
